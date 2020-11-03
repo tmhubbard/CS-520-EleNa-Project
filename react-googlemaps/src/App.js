85372139671b473787f2dcf5f23728259f6d1085
tomh12player <thomascheng@umass.edu>
2020-11-03 11:39:06 -0500

    single clickable marker working

--- react-googlemaps/src/App.js
+++ react-googlemaps/src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const mapStyles = {
+  width: '100%',
+  height: '100%'
+};
+
+export class MapContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isStartingMarkerShown: false,
+      startingMarkerPosition: null
+    };
+    this.handleMapClick = this.handleMapClick.bind(this);
+  }
+
+  handleMapClick = (ref, map, ev) => {
+    const location = ev.latLng;
+    this.setState({
+      isStartingMarkerShown: true,
+      startingMarkerPosition: location
+    });
+    map.panTo(location);
+  };
+
+
+
+  render() {
+    return (
+      <Map
+        google={this.props.google}
+        zoom={14}
+        style={mapStyles}
+        initialCenter={
+          {
+            lat: 42.3732,
+            lng: -72.5199
+          }
+        }
+        onClick={this.handleMapClick}
+      >
+        {this.state.isStartingMarkerShown && <Marker position={this.state.startingMarkerPosition} />}
+      </Map>
+    );
+  }
 }
 
-export default App;
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyCyrU7Z1OXQxOpvVMsQVk1FZvWWb3R3ssA'
+})(MapContainer);

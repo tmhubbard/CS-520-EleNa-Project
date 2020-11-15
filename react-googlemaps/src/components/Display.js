d283c4c1beed95ba31aba5482eb5926706df97f6
Umair <mumairafzal2010@gmail.com>
2020-11-15 18:06:23 -0500

    Frontend (#7)
    * single clickable marker working
    
    * marker is now draggable, state location of marker changes at end of drag
    
    * added second marker for end location, use right click to add the end marker
    
    * moved the .gitignore file up to the repo out of react-app folder, should hopefully ignore correctly now
    
    * removing node_modules
    
    * add .gitignore back to react + remove unneeded files
    
    * add yarn.lock
    
    * add yarn google maps
    
    * Created basic form and slider react components
    
    * first iteration of polyline working
    
    * combined map and inputForm components
    
    * slider passes value to inputform now
    
    * Created new parent component Display and implemented callbacks to access data from InputForm
    
    * MapComponent and Display send each other location updates
    
    * added google places autocomplete
    
    * Input start and end points gets updated by Display now
    
    Co-authored-by: tomh12player <thomascheng@umass.edu>
    Co-authored-by: DESKTOP-UUHP8L5\Rae <rpainter@umass.edu>

---
+++ react-googlemaps/src/components/Display.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import InputForm from './InputForm';
+import MapComponent from './MapComponent';
+
+class Display extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            startPoint: null,
+            endPoint: null,
+            elevationType: "min",
+            percentRoute: 10
+        };
+
+        this.handleMapStartChange = this.handleMapStartChange.bind(this);
+        this.handleMapEndChange = this.handleMapEndChange.bind(this);
+        this.handleInputStartChange = this.handleInputStartChange.bind(this);
+        this.handleInputEndChange = this.handleInputEndChange.bind(this);
+        this.handleInputTypeChange = this.handleInputTypeChange.bind(this);
+        this.handleInputPercentChange = this.handleInputPercentChange.bind(this);
+    }
+    //callback functions
+    handleMapStartChange(location) {
+        this.setState({startPoint : location});
+        console.log(location);
+    }
+
+    handleMapEndChange(location) {
+        this.setState({endPoint: location});
+        console.log(location);
+    }
+
+    handleInputStartChange(location) {
+        this.setState({startPoint: location});
+    }
+
+    handleInputEndChange(location) {
+        this.setState({endPoint : location});
+    }
+
+    handleInputTypeChange(type) {
+        this.setState({elevationType: type});
+        console.log(type);
+    }
+
+    handleInputPercentChange(percent) {
+        this.setState({percentRoute: percent});
+        console.log(percent);
+    }
+
+    render() {
+        return (
+            <div>
+                <MapComponent onStartChange = {this.handleMapStartChange} 
+                    onEndChange = {this.handleMapEndChange}
+                    startLocation = {this.state.startPoint}
+                    endLocation = {this.state.endPoint}/>
+
+                <InputForm onStartChange = {this.handleInputStartChange}
+                    onEndChange = {this.handleInputEndChange}
+                    onTypeChange = {this.handleInputTypeChange}
+                    onPercentChange = {this.handleInputPercentChange}
+                    startLocation = {this.state.startPoint}
+                    endLocation = {this.state.endPoint}
+                    style={{float: 'right'}} />
+            </div>
+        );
+    }
+}
+
+export default Display;
---
+++ react-googlemaps/src/components/InputForm.js
@@ -0,0 +1,213 @@
+import React from 'react';
+import Slider from './Slider';
+import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete';
+
+
+
+class InputForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            startPoint: null,
+            endPoint: null,
+            elevationType: "min",
+            percentRoute: 10,
+            startLatLng: null,
+            endLatLng: null,
+            startAddress: '',
+            endAddress: ''
+        };
+
+        this.handleStartAutocompleteChange = this.handleStartAutocompleteChange.bind(this);
+        this.handleStartSelect = this.handleStartSelect.bind(this);
+        this.handleEndAutocompleteChange = this.handleEndAutocompleteChange.bind(this);
+        this.handleEndSelect = this.handleEndSelect.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleSliderChange = this.handleSliderChange.bind(this);
+    }
+
+    handleStartAutocompleteChange = startAddress => {
+        this.setState({ startAddress });
+    };
+
+    handleStartSelect = async value => {
+        const results = await geocodeByAddress(value);
+        const latLng = await getLatLng(results[0]);
+        this.setState({
+            startAddress: value,
+            startLatLng: latLng
+        });
+        // console.log(this.state.startLatLng);
+    };
+
+    handleEndAutocompleteChange = endAddress => {
+        this.setState({ endAddress });
+    };
+
+    handleEndSelect = async value => {
+        const results = await geocodeByAddress(value);
+        const latLng = await getLatLng(results[0]);
+        this.setState({
+            endAddress: value,
+            endLatLng: latLng
+        });
+        // console.log(this.state.endLatLng);
+    };
+    
+      
+
+    handleChange(event) {
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+        //console.log(value);
+        this.setState({
+            [name]: value
+        });
+
+        if(name === "startPoint"){
+            this.props.onStartChange(value);
+        }
+        else if(name === "endPoint"){
+            this.props.onEndChange(value);
+        }
+        else if(name === "elevationType"){
+            this.props.onPercentChange(value);
+        }
+    }
+
+    handleSubmit(event) {
+        //console.log("form submitted");
+        event.preventDefault(); //prevents form from actually being submitted
+    }
+    
+    handleSliderChange(percent) {
+        this.setState({percentRoute: percent});
+        this.props.onPercentChange(percent);
+        //console.log(this.state.percentRoute);
+    }
+
+    receiveStartPoint(props) {
+        this.setState({startingMarkerPosition: props.startLatLng});
+      }
+    
+    receiveEndPoint(props) {
+        this.setState({endMarkerPosition: props.endLatLng});
+    }
+
+    render() {
+        return (
+            <div style={{float: 'left', width: '25%', height: '100%'}}>
+            <form>
+                <label>
+                    Start Point
+                    <br />
+                    <PlacesAutocomplete
+                        value={this.state.startAddress}
+                        onChange={this.handleStartAutocompleteChange}
+                        onSelect={this.handleStartSelect}
+                    >
+                        {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+                            <div>
+                                <input
+                                {...getInputProps({
+                                    placeholder: 'Search Places ...',
+                                    className: 'location-search-input',
+                                })}
+                                />
+                                <div className="autocomplete-dropdown-container">
+                                {loading && <div>Loading...</div>}
+                                {suggestions.map(suggestion => {
+                                    const className = suggestion.active
+                                    ? 'suggestion-item--active'
+                                    : 'suggestion-item';
+                                    // inline style for demonstration purpose
+                                    const style = suggestion.active
+                                    ? { backgroundColor: '#fafafa', cursor: 'pointer' }
+                                    : { backgroundColor: '#ffffff', cursor: 'pointer' };
+
+                                    return (
+                                    <div
+                                        {...getSuggestionItemProps(suggestion, {
+                                        className,
+                                        style,
+                                        })}
+                                    >
+                                        <span>{suggestion.description}</span>
+                                    </div>
+                                    );
+                                })}
+                                </div>
+                            </div>
+                            )}
+                    </PlacesAutocomplete>
+                </label>
+                <br />
+                <label>
+                    End Point
+                    <br />
+                    <PlacesAutocomplete
+                        value={this.state.endAddress}
+                        onChange={this.handleEndAutocompleteChange}
+                        onSelect={this.handleEndSelect}
+                    >
+                        {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+                            <div>
+                                <input
+                                {...getInputProps({
+                                    placeholder: 'Search Places ...',
+                                    className: 'location-search-input',
+                                })}
+                                />
+                                <div className="autocomplete-dropdown-container">
+                                {loading && <div>Loading...</div>}
+                                {suggestions.map(suggestion => {
+                                    const className = suggestion.active
+                                    ? 'suggestion-item--active'
+                                    : 'suggestion-item';
+                                    // inline style for demonstration purpose
+                                    const style = suggestion.active
+                                    ? { backgroundColor: '#fafafa', cursor: 'pointer' }
+                                    : { backgroundColor: '#ffffff', cursor: 'pointer' };
+
+                                    return (
+                                    <div
+                                        {...getSuggestionItemProps(suggestion, {
+                                        className,
+                                        style,
+                                        })}
+                                    >
+                                        <span>{suggestion.description}</span>
+                                    </div>
+                                    );
+                                })}
+                                </div>
+                            </div>
+                            )}
+                    </PlacesAutocomplete>
+                </label>
+                <br />
+                <label>
+                    Type of Route:
+                    <br />
+                    <input type="radio" id="min" name="elevationType" value="min" onChange={this.handleChange}/>
+                    <label htmlFor="min">Minimum Elevation</label>
+                    <input type="radio" id="max" name="elevationType" value="max" onChange={this.handleChange}/>
+                    <label htmlFor="max">Maximum Elevation</label>
+                </label>
+                <br />
+                <label>
+                    Distance (Percent of Route):
+                </label>
+                <div className="slidecontainer">
+                    <Slider onSliderChange = {this.handleSliderChange}/>
+                </div>
+                <input type="submit" value="Find Route" />
+            </form>
+            </div>
+        );
+    }
+}
+
+export default InputForm;

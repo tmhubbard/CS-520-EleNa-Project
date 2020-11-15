91e75b975b9a99e8d674a64b1f6c012dbf77dd75
Umair Afzal <mumairafzal2010@gmail.com>
2020-11-14 20:19:36 -0500

    add test for make graph and astar search

---
+++ backend/tests/make_graph_test.py
@@ -0,0 +1,24 @@
+import unittest
+import networkx as nx
+
+from model.graph.node import Node
+from model.graph.make_graph import make_graph
+
+class MakeGraphTest(unittest.TestCase):
+    
+    def test_astar_search(self):
+        """Test if a minimum path is returned on a directed graph.
+        """
+        # neighbors = (neighborID, distance)
+        list_of_node_objects = [
+            Node(id=1, elevation=10, neighbors=[(2, 2)]),
+            Node(id=2, elevation=11, neighbors=[(3, 2), (4, 4)]),
+            Node(id=3, elevation=12, neighbors=[(4, 4)]),
+            Node(id=4, elevation=14)
+        ]
+
+        G = make_graph(list_of_node_objects)
+        expected_shortest_path = [1, 2, 4]
+        shortest_path = nx.astar_path(G, 1, 4) 
+        assert shortest_path == expected_shortest_path, \
+            f'Expected path = {expected_shortest_path}, got {shortest_path}'

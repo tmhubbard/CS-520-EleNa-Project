01ae27bc3af3eec0016fefd4f6e4bc0dad01635e
Umair <mumairafzal2010@gmail.com>
2020-11-15 23:21:15 -0500

    Integrate v2 (#10)
    * add proxy to direct react to flask
    
    * return json response from flask
    
    * add requirements.txt
    
    * add code to accept data from fronted + fake data for test
    
    * add example data and curl command to test endpoint

--- backend/server.py
+++ backend/server.py
@@ -1,12 +1,38 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
 # import controller
 # import model
 
-app= Flask(__name__)
+app = Flask(__name__)
+CORS(app)
 
-@app.route('/getRoute', methods=['GET'])
-def test1():
-    return jsonify(value='Testing')
+
+# Command to test a post request
+    # curl -H "Content-type: application/json" -d '{ "origin_lat":"123", "min_max":"min" }' 'http://127.0.0.1:5000/getRoute'
+
+@app.route('/getRoute', methods=['POST'])
+def get_route():
+    # origin lat, long
+    # dest lat, long
+    # min/max
+    # percentage overhead
+    request_data = request.get_json(force=True, silent=False)
+
+    # PROCESS request_data
+
+    # FAKE DATA
+    response = {
+        "route": [
+            (42.389995, -72.528271), 
+            (42.389504, -72.528455), 
+            (42.390011, -72.528659)
+        ],
+        "total_elevation_gain": 24.5,
+        "total_distance_travelled": 40
+    }
+
+    return jsonify(**response)
 
 
 if __name__ == '__main__':

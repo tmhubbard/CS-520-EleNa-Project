4904dc9800f90ef79bd4eb37640ad33bcee4c5cc
Umair Afzal <mumairafzal2010@gmail.com>
2020-11-15 23:14:17 -0500

    add code to accept data from fronted + fake data for test

--- backend/server.py
+++ backend/server.py
@@ -1,12 +1,29 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from flask_cors import CORS
 # import controller
 # import model
 
-app= Flask(__name__)
+app = Flask(__name__)
+CORS(app)
 
-@app.route('/getRoute', methods=['GET'])
-def test1():
-    return jsonify(value='Testing')
+@app.route('/getRoute', methods=['POST'])
+def get_route():
+    request_data = request.get_json(force=True, silent=False)
+
+    # PROCESS request_data
+
+    # FAKE DATA
+    response = {
+        "route": [
+            (42.389995, -72.528271), 
+            (42.389504, -72.528455), 
+            (42.390011, -72.528659)
+        ],
+        "total_elevation_gain": 24.5,
+        "total_distance_travelled": 40
+    }
+    
+    return jsonify(**response)
 
 
 if __name__ == '__main__':

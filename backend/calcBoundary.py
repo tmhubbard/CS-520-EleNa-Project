f24e7c32698f16927132123353c7f372e3e5fb33
Umair Afzal <mumairafzal2010@gmail.com>
2020-11-23 20:38:05 -0500

    remove duplicate Node class

--- backend/calcBoundary.py
+++ backend/calcBoundary.py
@@ -10,87 +10,12 @@ from math import sin, cos, pi, atan
 import numpy as np
 import matplotlib.pyplot as plt
 import googlemaps
+from model.graph.node import Node
 
 # Setting up the Google Maps instance
 apiKey = "AIzaSyC0_EhM25ltUK20oJPH4k4Ni6jqiU4bS2Q"
 gmaps = googlemaps.Client(key=apiKey)
 
-# =========================
-#        * CLASSES *
-# =========================
-
-class Node:
-    """
-    Node class represets a node in a Graph
-    """
-    def __init__(
-            self, 
-            id: int, 
-            latitude: float = None, longitude: float = None, 
-            elevation: float = None,
-            neighbors: list = None):
-        self._id = id
-        self._latitude = latitude
-        self._longitude = longitude
-        self._elevation = elevation
-        self._neighbors = neighbors # [(neighborID, distance), (,), ...]
-    
-    @property
-    def id(self) -> int:
-        return self._id
-    
-    @id.setter
-    def id(self, id: int):
-        self._id = id
-    
-    @property
-    def latitude(self) -> float:
-        return self._latitude
-    
-    @latitude.setter 
-    def latitude(self, latitude: float):
-        self._latitude = latitude
-    
-    @property
-    def longitude(self) -> float:
-        return self._longitude
-    
-    @longitude.setter 
-    def longitude(self, longitude: float):
-        self._longitude = longitude
-
-    @property
-    def elevation(self) -> float:
-        return self._elevation
-    
-    @elevation.setter 
-    def elevation(self, elevation: float):
-        self._elevation = elevation
-
-    @property
-    def neighbors(self) -> list:
-        return self._neighbors
-    
-    @neighbors.setter 
-    def neighbors(self, neighbors: list):
-        self._neighbors = neighbors
-    
-    def as_json(self) -> dict:
-        return {
-            "id": self._id,
-            "latitude": self._latitude,
-            "longitude": self._longitude,
-            "elevation": self._elevation
-        }
-    
-    def get_content(self) -> dict:
-        return {
-            "id": self._id,
-            "latitude": self._latitude,
-            "longitude": self._longitude,
-            "elevation": self._elevation,
-            "neighbors": self._neighbors,
-        }
 
 # =========================
 #       * METHODS *
